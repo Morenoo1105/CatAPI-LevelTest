f363c85bd76a88c00454737631a465c5b030bd2d
Pablo Moreno <pablo_moreno_martin@hotmail.com>
2023-10-19 18:22:10 +0200

    Prueba de Nivel Realizada
    Realizada prueba de nivel a falta de crear estilos

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# CatAPI-LevelTest
- Prueba de nivel de React utilizando 2 APIs
+# Prueba técnica de React
+
+Recupera un dato aleatorio de gatos de la primera API y muestra una imagen de un gato con la primera palabra del hecho recuperado usando la segunda API.
+
+APIs:
+
+- Datos Aleatorios: https://catfact.ninja/fact
+- Imágenes Aleatorias: https://cataas.com/cat/says/hello
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const CAT_FACT_API_URL = "https://catfact.ninja/fact";
+const CAT_URL_BASEURL = "https://cataas.com/cat/";
+const CAT_URL_PROPERTIES = "?fontSize=50&fontColor=red";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fact, setFact] = useState();
+  const [firstWord, setFirstWord] = useState();
+  const [imageID, setImageID] = useState();
+
+  /*
+   Se utiliza un primer useEffect para recuperar el dato sobre el gato
+   utilizando la primera API (CAT_FACT_API_URL)
+  */
+  useEffect(() => {
+    fetch(CAT_FACT_API_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        const { fact } = data;
+        setFact(fact);
+      });
+  }, []);
+
+  /*
+   Se utiliza un segundo useEffect que opera tras el cambio de la variable "fact".
+   Se comprueba que dicha variable no está vacía y se separa la primera palabra del resto
+   del dato obtenido. Entonces se usa la segunda API (combinando CAT_URL_BASEURL y CAT_URL_PROPERTIES)
+   para obtener la imagen con el texto en ella.
+  */
+
+  useEffect(() => {
+    if (!fact) return;
+
+    setFirstWord(fact.split(" ")[0]);
+    fetch(`https://cataas.com/cat/says/${firstWord}?json=true`)
+      .then((res) => res.json())
+      .then((response) => {
+        const { _id } = response;
+        setImageID(_id);
+      });
+  }, [fact]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <h1>helo</h1>
+      {fact && <p>{fact}</p>}
+      {imageID && (
+        <img
+          src={`${CAT_URL_BASEURL}${imageID}/says/${firstWord}${CAT_URL_PROPERTIES}`}
+          alt={`Image extracted using the first word of ${fact}`}
+        />
+      )}
+    </main>
+  );
 }
 
-export default App
+export default App;
