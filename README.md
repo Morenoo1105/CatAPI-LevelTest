c6fceabf784da82d43266d0ddcdf7affeb96709f
Pablo Moreno <pablo_moreno_martin@hotmail.com>
2023-11-08 18:34:33 +0100

    añadida traduccion

--- README.md
+++ README.md
@@ -2,12 +2,16 @@
 
 ![Resultado final](https://morenno.net/demoWebs/img_thumbnails/CatAPI.png)
 
-
-
 ## Enunciado
-Recupera un dato aleatorio de gatos de la primera API y muestra una imagen de un gato con la primera palabra del hecho recuperado usando la segunda API.
+
+Recupera un dato aleatorio de gatos de la primera API y tradúcelo al castellano. Muestra entonces una imagen de un gato con la primera palabra del hecho recuperado usando la segunda API.
+
+Después, añade la funcionalidad de introducir un texto y que se genere una imagen con dicho texto en vez de la primera palabra del hecho.
+
+Muestra un histórico de las imágenes y datos en un componente diferente.
 
 APIs:
 
 - Datos Aleatorios: https://catfact.ninja/fact
+
 - Imágenes Aleatorias: https://cataas.com/cat/says/hello
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 
+const TRANSLATOR_API_URL =
+  "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=es&dt=t&q=";
 const CAT_FACT_API_URL = "https://catfact.ninja/fact";
 const CAT_URL_BASEURL = "https://cataas.com/cat/";
-const CAT_URL_PROPERTIES = "?fontSize=50&fontColor=orange";
+const CAT_URL_PROPERTIES = "?fontSize=40&fontColor=orange";
 
 function App() {
-  const [fact, setFact] = useState();
+  const [fact, setFact] = useState("Loading");
+  const [number, setNumber] = useState(0);
+  const [spanish, setSpanish] = useState("");
+
   const [firstWord, setFirstWord] = useState();
   const [imageID, setImageID] = useState();
 
@@ -16,37 +21,56 @@ function App() {
   */
   useEffect(() => {
     fetch(CAT_FACT_API_URL)
-      .then((res) => res.json())
+      .then((response) => response.json())
       .then((data) => {
         const { fact } = data;
         setFact(fact);
+        setNumber(parseInt(Math.random() * 999));
       });
   }, []);
 
   /*
    Se utiliza un segundo useEffect que opera tras el cambio de la variable "fact".
-   Se comprueba que dicha variable no está vacía y se separa la primera palabra del resto
-   del dato obtenido. Entonces se usa la segunda API (combinando CAT_URL_BASEURL y CAT_URL_PROPERTIES)
-   para obtener la imagen con el texto en ella.
+   Se comprueba que dicha variable no está vacía y se envía el texto a la API de Google Translate codificado para URL.
   */
-
   useEffect(() => {
     if (!fact) return;
 
-    setFirstWord(fact.split(" ")[0]);
+    // Este replace soluciona el hecho de que el encoding se pare a mitad de una frase o cuando hay un punto y seguido en la oración.
+    const query = fact
+      .replace(".", " ")
+      .replace(".", " ")
+      .replace(".", " ")
+      .replace(".", " ");
+
+    fetch(TRANSLATOR_API_URL + encodeURI(query))
+      .then((response) => response.json())
+      .then((data) => setSpanish(data[0][0][0]));
+  }, [fact]);
+
+  /*
+   Se utiliza un tercer useEffect que opera tras el cambio de la variable "spanish".
+   Se comprueba que dicha variable no está vacía y se separa la primera palabra del resto
+   del dato obtenido. Entonces se usa la tercera API (combinando CAT_URL_BASEURL y CAT_URL_PROPERTIES)
+   para obtener la imagen con el texto en ella.
+  */
+  useEffect(() => {
+    if (!spanish) return;
+
+    setFirstWord(spanish.split(" ").slice(0, 2).join(" "));
     fetch(`https://cataas.com/cat/says/${firstWord}?json=true`)
       .then((res) => res.json())
       .then((response) => {
         const { _id } = response;
         setImageID(_id);
       });
-  }, [fact]);
+  }, [spanish]);
 
   return (
     <main>
       <h1>Random cat facts!</h1>
-      <h2>#{parseInt(Math.random() * 123)}</h2>
-      {fact && <p>{fact}</p>}
+      <h2>#{number}</h2>
+      {spanish && <p>{spanish}</p>}
       {imageID && (
         <img
           src={`${CAT_URL_BASEURL}${imageID}/says/${firstWord}${CAT_URL_PROPERTIES}`}
