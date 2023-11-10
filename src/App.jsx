131ec3d73893d94cbec715963ddcd7d53a8686b4
Pablo Moreno <pablo_moreno_martin@hotmail.com>
2023-11-10 17:43:31 +0100

    Cats! (Again)

--- src/App.jsx
+++ src/App.jsx
@@ -38,61 +38,6 @@ function App() {
         ]);
   }, [imageID]);
 
-  // /*
-  //  Se utiliza un primer useEffect para recuperar el dato sobre el gato
-  //  utilizando la primera API (CAT_FACT_API_URL)
-  // */
-  // useEffect(() => {
-  //   fetch(CAT_FACT_API_URL)
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       const { fact } = data;
-  //       setCat(fact);
-  //     });
-  // }, []);
-
-  // /*
-  //  Se utiliza un segundo useEffect que opera tras el cambio de la variable "fact".
-  //  Se comprueba que dicha variable no está vacía y se envía el texto a la API de Google Translate codificado para URL.
-  // */
-  // useEffect(() => {
-  //   if (!cat) return;
-
-  //   // Este replace soluciona el hecho de que el encoding se pare a mitad de una frase o cuando hay un punto y seguido en la oración.
-  //   const query = cat
-  //     .replace(".", " ")
-  //     .replace(".", " ")
-  //     .replace(".", " ")
-  //     .replace(".", " ");
-
-  //   fetch(TRANSLATOR_API_URL + encodeURI(query))
-  //     .then((response) => response.json())
-  //     .then((data) => setSpanish(data[0][0][0]));
-  // }, [cat]);
-
-  // // useEffect(() => {
-  // //   historyItem.sentence = fact;
-  // //   historyItem.img = "_id";
-  // // }, [fact]);
-
-  // /*
-  //  Se utiliza un tercer useEffect que opera tras el cambio de la variable "spanish".
-  //  Se comprueba que dicha variable no está vacía y se separa la primera palabra del resto
-  //  del dato obtenido. Entonces se usa la tercera API (combinando CAT_URL_BASEURL y CAT_URL_PROPERTIES)
-  //  para obtener la imagen con el texto en ella.
-  // */
-  // useEffect(() => {
-  //   if (!spanish) return;
-
-  //   setFirstWord(spanish.split(" ").slice(0, 2).join(" "));
-  //   fetch(`https://cataas.com/cat/says/${firstWord}?json=true`)
-  //     .then((res) => res.json())
-  //     .then((response) => {
-  //       const { _id } = response;
-  //       setImageID(_id);
-  //     });
-  // }, [spanish]);
-
   const [darkMode, setDarkMode] = useState(true);
 
   useEffect(() => {
--- src/functions/getRandomCat.js
+++ src/functions/getRandomCat.js
@@ -6,19 +6,37 @@ const TRANSLATOR_API_URL =
 const CAT_URL_BASEURL = "https://cataas.com/cat/";
 
 export const getRandomCat = (setSpanish, setImageID, setFirstWord) => {
+  /*
+   * Se utiliza un primer fetch para recuperar el dato sobre el gato
+   * utilizando la primera API (CAT_FACT_API_URL)
+   */
+
   fetch(CAT_FACT_API_URL)
     .then((response) => response.json())
     .then(({ fact }) => {
       const engFact = fact.replace(/\./g, "%555");
 
+      /*
+       * Se utiliza un segundo fetch para traducir el dato sobre el gato al castellano
+       * utilizando la segunda API (TRANSLATOR_API_URL).
+       */
+
       fetch(TRANSLATOR_API_URL + encodeURI(engFact))
         .then((responseTranslate) => responseTranslate.json())
         .then((dataTranslate) => {
+          // Este replace soluciona el hecho de que el encoding se pare a mitad de una frase o cuando hay un punto y seguido en la oración.
+
           const espFact = dataTranslate[0][0][0].replace(/%555/g, ".");
           setSpanish(espFact);
 
           const firstWord = espFact.split(" ").slice(0, 2).join(" ");
           setFirstWord(firstWord);
+
+          /*
+           * Se utiliza un tercer fetch para obtener la ID de la fotografía del gato
+           * utilizando la tercera API (CAT_URL_BASEURL).
+           */
+
           fetch(`${CAT_URL_BASEURL}says/${firstWord}?json=true`)
             .then((responseImg) => responseImg.json())
             .then(({ _id }) => {
@@ -34,6 +52,13 @@ export const getTextCat = (form, setSpanish, setImageID, setFirstWord) => {
 
   setSpanish(word);
   setFirstWord(word);
+
+  /*
+   * Se utiliza un fetch para recuperar la ID de una imagen de gato
+   * utilizando la tercera API (CAT_URL_BASEURL), utilizando también
+   * la/las palabras introducidas en el campo de texto.
+   */
+
   fetch(`${CAT_URL_BASEURL}says/${word}?json=true`)
     .then((responseImg) => responseImg.json())
     .then(({ _id }) => {
